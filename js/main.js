a48d66315ab5f650c3c71270efff65e7e2d5df2b
José Antonio <antoniosanchezalanis@gmail.com>
2023-05-24 01:56:32 -0600

    Modificación de funciones y agregado de otras
    Subido por José Antonio
    
    Se hizo modificaciones de las funciones antes hechas y se agregaron nuevas como la de limpieza de texto

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
 function obtenerTexto(){
-    // Obtenemos componentes a los que cambiaremos el estilo
-    var txtSalida = document.getElementById('txtSalida');
-    var contDer = document.getElementById('txtAreaSalida');
-    var btnCopy = document.getElementById('btnCopiar');
-
-    // Cambiamos su estilo
-    txtSalida.style.cssText = 'align-items:initial; justify-content: left';
-    contDer.style.cssText = 'text-align: left;';
-    btnCopy.style.cssText = 'width: 8%;';
-
     // Obtenemos y retornamos el texto de entrada
     return document.getElementById('txtEntrada').value;
+}
+
+function modificarCSS(){
+    // Obtenemos componentes a los que cambiaremos el estilo
+    const txtSalida = document.getElementById('txtSalida');
+    const btnCopy = document.getElementById('copiar');
+
+    // Cambiamos su estilo
+    txtSalida.style.cssText = 'align-items:initial; justify-content: left;';
+    btnCopy.style.cssText = 'display: flex;';
+}
+
+function verificar(cadena){
+    // Testeamos la cadena para verificar si hay mayusculas, caracteres expeciales, acentos o si es la cadena esta vacia
+    if(cadena===''){
+        alert('No ha introducido alguna letra.');
+        return true;
+    } 
+    if(/[^a-z\s]/.test(cadena)){
+        alert('El texto contiene alguna letra mayúscula, acento o carácter especial.');
+        return true;
     }
+    return false;
+}
 
 function getValor(diccionario, variable){
     // Obtenemos ya sea palabra o letra del diccionario
-    if(variable.length=1){
+    if(variable.length==1)
         return diccionario.find(valor => valor.letra === variable).palabra
-    }else{
+    else
         return diccionario.find(valor => valor.palabra === variable).letra
-    }
 }
 
 // Creamos el diccionario de palabras y letras
@@ -34,48 +46,85 @@ function encriptar(){
     // Obtenemos texto a encriptar
     const texto = obtenerTexto();
     
+    if(verificar(texto))
+        return;   
+
+    modificarCSS();
+    
     // Creamos cadena donde se almacenara el string encriptado
-    var cadF = '';
+    let cadF = '';
 
     // Realizamos la encriptacion con ayuda del diccionario
     for(let i=0 ; i<texto.length ; i++){
-        if(diccionario.find(valor => valor.letra === texto[i])){
+        if(diccionario.find(valor => valor.letra === texto[i]))
             cadF += getValor(diccionario, texto[i]);
-        }else{
+        else{
             cadF += texto[i];
         }
     }
-    // Escribimos el string encriptado en el area de salida
-    document.getElementById('txtAreaSalida').innerHTML=''+ cadF;
+    cadF = cadF.replaceAll('\n', '<br>');
+    console.log(cadF);
+    document.getElementById('txtSalida').innerHTML=''+ cadF;
+    
 }
 
 function desencriptar(){
     // Obtenemos texto a encriptar
-    var texto = obtenerTexto();
+    let texto = obtenerTexto();
+
+    if(verificar(texto))
+        return;
+    
+    modificarCSS();
 
     // Realizamos la desencriptacion con ayuda del diccionario
-    
     for(let i=0 ; i<diccionario.length ; i++){
         texto = texto.replaceAll(diccionario[i].palabra, diccionario[i].letra);
     }
+    texto = texto.replaceAll('\n', '<br>');
+
     // Escribimos el string desencriptado en el area de salida
-    document.getElementById('txtAreaSalida').innerHTML=''+ texto;
+    document.getElementById('txtSalida').innerHTML=''+ texto;
 }
 
 function copiar(){       
-    // Extraemos el texto del area de salida
-    let copyText = document.getElementById('txtAreaSalida').innerText
+    // Obtenemos el componente del texto a extraer
+    let copyText = document.getElementById('txtSalida').innerText
     
     // Creamos un input para poder copiar el texto dentro
     const textArea = document.createElement('textarea');
     
-    // Guardamos en el mismo el texto a copiar.
+    // Guardamos en el input el texto a copiar.
     textArea.textContent = copyText;
     document.body.append(textArea); 
     
     // Seleccionamos el contenido del mismo.      
     textArea.select();       
-    document.execCommand("copy");       
+    document.execCommand('copy');       
     // Borramos el textarea que creamos       
     textArea.remove() 
+    alert('Texto copiado!');
   }
+
+function limpiar(){
+    // Obtenemos el componente del texto a limpia
+    const btnClean = document.getElementById('limpiar');
+    // Limpiamos el texto de la parte de entrada
+    document.getElementById('txtEntrada').value = "";
+    // Escondemos el componente
+    btnClean.style.cssText = 'display: none;';
+}
+
+function esconderBtn(){
+    // Obtenemos los componentes a esconder
+    let cad = document.getElementById('txtEntrada').value;
+    const btnClean = document.getElementById('limpiar');
+    
+    // Condicionamos su escondite
+    if(cad==='')
+        btnClean.style.cssText = 'display: none;';
+    else
+        btnClean.style.cssText = 'display: flex;';
+
+
+}
